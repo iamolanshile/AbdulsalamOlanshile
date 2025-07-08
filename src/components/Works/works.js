b572d353efeca70bcce4b0eb92c6802cbf618417
iamolanshile <81174693+iamolanshile@users.noreply.github.com>
2025-07-08 14:29:06 +0100

    Update works.js

--- src/components/Works/works.js
+++ src/components/Works/works.js
@@ -1,28 +1,35 @@
-import React from 'react'
-import "./works.css"
-import Portfolio1 from "../../assets/portfolio-1.png";
-import Portfolio2 from "../../assets/portfolio-2.png";
-import Portfolio3 from "../../assets/portfolio-3.png";
-import Portfolio4 from "../../assets/portfolio-4.png";
-import Portfolio5 from "../../assets/portfolio-5.png";
-import Portfolio6 from "../../assets/portfolio-6.png";
+import React from "react";
+import "./works.css";
+import Portfolio1 from "../../assets/react.png";
+import Portfolio2 from "../../assets/java.png";
+import Portfolio3 from "../../assets/wordpress.png";
+import Portfolio4 from "../../assets/hubspot.jpg";
+import Portfolio5 from "../../assets/css.jpg";
+import Portfolio6 from "../../assets/html.png";
+import { Link } from "react-scroll";
 
 const Works = () => {
-    return (
-        <section id="works">
-            <h2 className="worksTitle"> My Portfolio</h2>
-            <span className="worksDesc"> I take pride in paying attention to the smallest details and making sure that my work is pixel perfect. I am excited to bring my skills and experience to help business achieve their goals and create a strong online presence.</span>
-            <div className="worksImgs">
-                <img src={Portfolio1} alt="PPortfolio1" className="worksImg" />
-                <img src={Portfolio2} alt="ortfolio2" className="worksImg" />
-                <img src={Portfolio3} alt="Portfolio3" className="worksImg" />
-                <img src={Portfolio4} alt="Portfolio4" className="worksImg" />
-                <img src={Portfolio5} alt="Portfolio5" className="worksImg" />
-                <img src={Portfolio6} alt="Portfolio6" className="worksImg" />
-            </div>
-            <button className="workBtn">See More</button>
-        </section>
-    )
-}
+  return (
+    <section id="works">
+      <h2 className="worksTitle">My Portfolio</h2>
+      <span className="worksDesc">
+        I specialize in building pixel-perfect, responsive web applications
+        using modern technologies like React, Next.js, and WordPress, helping
+        businesses achieve their goals with a strong online presence.
+      </span>
+      <div className="worksImgs">
+        <img src={Portfolio1} alt="React Project" className="worksImg" />
+        <img src={Portfolio2} alt="Java Project" className="worksImg" />
+        <img src={Portfolio3} alt="WordPress Project" className="worksImg" />
+        <img src={Portfolio4} alt="HubSpot Project" className="worksImg" />
+        <img src={Portfolio5} alt="CSS Project" className="worksImg" />
+        <img src={Portfolio6} alt="HTML Project" className="worksImg" />
+      </div>
+      <Link to="contact" smooth={true} duration={500}>
+        <button className="workBtn">See More</button>
+      </Link>
+    </section>
+  );
+};
 
-export default Works
+export default Works;
